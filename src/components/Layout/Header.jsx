67e61b7e142e339eaa7e554d283ed1e2e64ab9db
dooya8787 <dooya8787@dooya8787ui-MacBookPro.local>
2025-12-26 03:55:44 +0900

    Add favicon and fix domain settings

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     <header className="bg-gray-800 py-4 px-6 fixed w-full z-50 shadow-lg">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-2">
-          <img src="/nusu-logo.png" alt="부산 누수방지" className="h-12 w-auto object-contain" />
+          <img src="./nusu-logo.png" alt="부산 누수방지" className="h-12 w-auto object-contain" />
         </Link>
         <nav className="hidden md:flex space-x-8">
           {navItems.map((item) => (
